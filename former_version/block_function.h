2b7890c29452d2bef65c6a33030d7c2479b2568a
Harry Chang <harry878710@gmail.com>
2024-12-12 18:15:46 -0800

    [Comparison Version] hardware codes

---
+++ former_version/block_function.h
@@ -0,0 +1,15 @@
+// block_function.h
+#ifndef BLOCK_FUNCTION_H
+#define BLOCK_FUNCTION_H
+
+#include "resnet18.h"
+#include "resnet18_weights.h" // Quantized weights and BN parameters
+#include "conv.h"
+#include "bn.h"
+#include "ReLU.h"
+#include "skipConnect.h"
+#include "quantize.h"
+
+void run_resnet_block(const data_t* input, data_t* output, int in_h, int in_w, int in_ch, int out_ch);
+
+#endif
